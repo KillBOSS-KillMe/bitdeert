cbe475cfe15f022f69ec1ef9e76f3cf593bcbb7d
429175116 <429175116@qq.com>
2020-05-27 00:31:59 +0800

    账号设置页完成

--- src/components/accountCenter/accountCenterSys.vue
+++ src/components/accountCenter/accountCenterSys.vue
@@ -1,74 +1,95 @@
 <template>
   <div class="abilityPage">
-    <div class="head">
-      <div class="headTitle">
-        <i class="el-icon-info"></i>
-        <div class="info">
-          <h4>BTC</h4>
-          <span>0个套餐正在挖矿</span>
-        </div>
+    <ul class="sysList">
+      <li>
+        <label>登录密码</label>
+        <span>已设定</span>
+        <button @click="passwordModel = true">修改</button>
+      </li>
+      <li>
+        <label>手机账号</label>
+        <span>86-18700458359</span>
+        <button @click="phoneModel = true">修改</button>
+      </li>
+      <li>
+        <label>邮箱账号</label>
+        <span>未设定</span>
+        <button @click="mailboxModel = true">去绑定</button>
+      </li>
+    </ul>
+    <ul class="sysList">
+      <li>
+        <label>谷歌身份验证器</label>
+        <span>用于添加钱包地址时安全校验</span>
+        <button @click="googleIdModel = true">去绑定</button>
+      </li>
+    </ul>
+    <el-dialog
+      title="登录密码"
+      :visible.sync="passwordModel"
+      width="20%"
+      center
+      :before-close="handleClose"
+    >
+      <div class="formModel">
+        <el-input placeholder="请输入内容" v-model="input10" clearable></el-input>
+        <el-input placeholder="请输入内容" v-model="input10" clearable></el-input>
+        <el-button type="info" plain>确认</el-button>
       </div>
-    </div>
-    <div class="dataInfo">
-      <div class="allCalculation">
-        <i class="modelIcon el-icon-info"></i>
-        <div class="info">
-          <span class="title">总算力</span>
-          <div class="data">
-            <div>
-              0000
-              <span>TH/s</span>
-            </div>
-          </div>
-          <div class="botton">矿池分配</div>
-        </div>
+    </el-dialog>
+    <el-dialog
+      title="手机账号"
+      :visible.sync="phoneModel"
+      width="20%"
+      center
+      :before-close="handleClose"
+    >
+      <div class="formModel">
+        <label>当前手机号</label>
+        <el-input placeholder="当前手机号" v-model="input10" :disabled="true" clearable></el-input>
+        <el-input placeholder="短信验证码" v-model="input4">
+          <template slot="append">发送验证码</template>
+        </el-input>
+        <label>新的手机号</label>
+        <el-input placeholder="请输入新的手机号" v-model="input5" class="input-with-select">
+          <el-select v-model="select" slot="prepend" placeholder="请选择">
+            <el-option label="+86" value="1"></el-option>
+          </el-select>
+        </el-input>
+        <el-input placeholder="短信验证码" v-model="input4">
+          <template slot="append">发送验证码</template>
+        </el-input>
+        <el-button type="info" plain>确认修改</el-button>
       </div>
-      <div class="allOutput">
-        <i class="modelIcon el-icon-info"></i>
-        <div class="info">
-          <span class="title">
-            总算力
-            <i class="el-icon-info"></i>
-          </span>
-          <div class="data">
-            <div>
-              0000
-              <span>TH/s</span>
-            </div>
-            <span class="more">更多产出数据></span>
-          </div>
-          <div class="price">≈ $0.00</div>
-        </div>
+    </el-dialog>
+    <el-dialog
+      title="邮箱账号"
+      :visible.sync="mailboxModel"
+      width="20%"
+      center
+      :before-close="handleClose"
+    >
+      <div class="formModel">
+        <el-input placeholder="请输入邮箱" v-model="input10" clearable></el-input>
+        <el-input placeholder="邮箱验证码" v-model="input4">
+          <template slot="append">发送验证码</template>
+        </el-input>
+        <el-button type="info" plain>确认绑定</el-button>
       </div>
-    </div>
-    <div class="listCon">
-      <div class="titleCon">
-        <div class="title">
-          <i class="el-icon-info"></i>
-          算力列表
-        </div>
-        <ul class="titlelist">
-          <li>全部</li>
-          <li>待生效</li>
-          <li>生效中</li>
-          <li>已结束</li>
-        </ul>
-      </div>
-      <el-table :data="tableData3" height="550" border style="width: 100%">
-        <el-table-column prop="date" label="相应订单号" width="180"></el-table-column>
-        <el-table-column prop="name" label="算力信息" width="180"></el-table-column>
-        <el-table-column prop="address" label="当前矿池"></el-table-column>
-        <el-table-column prop="address" label="剩余电费天数(天)"></el-table-column>
-        <el-table-column prop="address" label="累计产出"></el-table-column>
-        <el-table-column prop="address" label="状态"></el-table-column>
-        <el-table-column prop="address" label="操作"></el-table-column>
-      </el-table>
-      <div class="notList">
-        <i class="el-icon-info"></i>
-        <span>暂无数据...</span>
-        <div>购买框架共享套餐</div>
-      </div>
-    </div>
+    </el-dialog>
+    <el-dialog
+      title="绑定谷歌身份验证器"
+      :visible.sync="googleIdModel"
+      width="30%"
+      center
+      :before-close="handleClose"
+    >
+      <span>绑定谷歌身份验证器</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="googleIdModel = false">取 消</el-button>
+        <el-button type="primary" @click="googleIdModel = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -76,43 +97,10 @@
 export default {
   data() {
     return {
-      tableData3: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-08",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-06",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }
-      ]
+      passwordModel: false,
+      phoneModel: false,
+      mailboxModel: false,
+      googleIdModel: false
     };
   },
   methods: {
@@ -121,6 +109,14 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    handleClose(done) {
+      done();
+      // this.$confirm('确认关闭？')
+      //   .then(_ => {
+      //     done();
+      //   })
+      //   .catch(_ => {});
     }
   }
 };
@@ -130,219 +126,63 @@ export default {
   width: 1250px;
   height: auto;
   padding: 10px;
-  border: 1px solid #333;
-
-  .head {
+  .sysList {
     width: 100%;
-    height: 60px;
-    margin-bottom: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    background-color: #fafafa;
-    box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
-    .headTitle:hover {
-      box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
-    }
-    .headTitle {
-      width: 200px;
-      height: 58px;
-      padding: 0 20px;
-      border-bottom: 3px solid #5c82ff;
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-      i {
-        font-size: 30px;
-        color: rgb(255, 157, 50);
-      }
-      .info {
-        margin-left: 10px;
-        display: flex;
-        align-items: flex-start;
-        justify-content: space-between;
-        flex-direction: column;
-        h4 {
-          font-size: 20px;
-          font-weight: 700;
-          color: #333;
-        }
-        span {
-          font-size: 12px;
-          color: #999;
-        }
-      }
-    }
-  }
-  .dataInfo {
-    width: 100%;
-    height: 190px;
+    height: auto;
+    padding: 0 20px;
     margin-bottom: 20px;
-    background-color: #fafafa;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
+    background-color: #fff;
     box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
-    .allCalculation,
-    .allOutput {
-      height: 190px;
-      padding: 30px 20px;
+    li {
+      width: 100%;
+      height: auto;
+      padding: 20px 0;
+      border-bottom: 1px solid #ddd;
       display: flex;
       align-items: center;
       justify-content: flex-start;
-      .modelIcon {
-        width: 60px;
-        height: 60px;
-        border-radius: 60px;
-        margin: 0 30px;
-        font-size: 18px;
+      label {
+        width: 152px;
+        font-size: 16px;
+        color: #1c1f28;
+      }
+      span {
+        width: 270px;
+        font-size: 14px;
+        color: #999;
+        margin-right: 20px;
+        overflow-x: hidden;
+      }
+      button {
         display: flex;
         align-items: center;
         justify-content: center;
-        background: rgba(255, 152, 0, 0.1);
-        color: #ff9800;
+        width: 100px;
+        padding: 9px 15px;
+        font-size: 12px;
+        border-radius: 3px;
+        color: #fff;
+        background-color: #5c82ff;
+        border-color: #5c82ff;
+        border: 0;
       }
-      .info {
-        // width: 370px;
-        height: 100%;
-        display: flex;
-        align-items: flex-start;
-        justify-content: space-between;
-        flex-direction: column;
-        .title {
-          font-size: 14px;
-          color: #999;
-          // margin-top: 30px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          i {
-            font-size: 14px;
-            color: #999;
-            margin-left: 10px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-        }
-        .data {
-          width: 100%;
-          font-size: 50px;
-          // font-weight: 600;
-          color: #333;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          span {
-            font-size: 25px;
-          }
-          .more {
-            font-size: 14px;
-            color: #5c82ff;
-          }
-        }
-        .price {
-          color: #999;
-          font-size: 14px;
-        }
-      }
-    }
-    .allCalculation {
-      width: 430px;
-      .botton {
-        width: 80px;
-        height: 34px;
-        color: #5c82ff;
-        border: 1px solid #5c82ff;
-        text-align: center;
-        border-radius: 4px;
-        font-size: 14px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    }
-    .allOutput {
-      width: 820px;
-      box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
-      .info {
-        width: 633px;
-      }
-      .data {
-        border-bottom: 1px solid #ddd;
-      }
-    }
-  }
-  .listCon {
-    width: 100%;
-    height: auto;
-    background-color: #fafafa;
-    .titleCon {
-      width: 100%;
-      padding: 20px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      .title {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        font-size: 14px;
-        color: #333;
-        i {
-          font-size: 20px;
-          color: #999;
-          margin-right: 10px;
-        }
-      }
-      .titlelist {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        li {
-          font-size: 14px;
-          color: #999;
-          padding: 0 10px;
-        }
-        .active {
-          color: #333;
-        }
-      }
-    }
-  }
-  .notList {
-    width: 100%;
-    height: auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: column;
-    margin-top: 50px;
-    i {
-      font-size: 160px;
-      color: #ddd;
-    }
-    span {
-      font-size: 14px;
-      color: #999;
-    }
-    div {
-    color: #5c82ff;
-    border: 1px solid #5c82ff;
-    font-size: 14px;
-    border-radius: 4px;
-    padding: 10px;
-    margin: 15px 0;
     }
   }
 }
-.tac {
-  overflow: hidden;
-  width: 10%;
-  .el-col {
-    width: 100%;
+.formModel {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  label {
+    font-size: 12px;
+    color: #333;
+    margin-bottom: 10px;
   }
-  .el-menu-item {
+  .el-input {
+    margin-bottom: 20px;
+  }
+  .el-button {
     width: 100%;
   }
 }
