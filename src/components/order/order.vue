cf82724eee490c46e39ef23eeba3df9695a68d5a
429175116 <429175116@qq.com>
2020-06-20 21:29:16 +0800

    -

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -24,7 +24,7 @@
               </el-select>
               <span class="change-tip">BTC</span>
             </el-form-item>
-            <el-form-item label="机型">
+            <!-- <el-form-item label="机型">
               <span class="form-label-txt">{{ Oform.models }}</span>
             </el-form-item>
             <el-form-item label="算力">
@@ -95,7 +95,43 @@
                 </section>
                 <div class="change-tip">订单生效之后可以再修改矿池</div>
               </div>
-            </el-form-item>
+            </el-form-item>-->
+            <ul class="infoDom">
+              <li>
+                <span>总产量：100.00BTC</span>
+                <span>日产量：100.00BTC</span>
+              </li>
+              <li>
+                <span>总成本：100.00</span>
+                <span>日成本：100.00</span>
+              </li>
+              <li>
+                <span>总收益：100.00</span>
+                <span>日收益：100.00</span>
+              </li>
+              <li>
+                <span>剩余时间：100.00天</span>
+                <span>回本周期：100.00天</span>
+              </li>
+            </ul>
+            <ul class="chartsDom">
+              <li>
+                <h4>总份额图表</h4>
+                <div id="allShareChartsID">图表内容</div>
+              </li>
+              <li>
+                <h4>产品份额</h4>
+                <div id="productShareChartsID">图表内容</div>
+              </li>
+              <li>
+                <h4>在线Pos值</h4>
+                <div id="onlinePosChartsID">图表内容</div>
+              </li>
+              <li>
+                <h4>产品Pos值</h4>
+                <div id="productPosChartsID">图表内容</div>
+              </li>
+            </ul>
             <el-form-item label="收款方式">
               <div class="receive-way-box">
                 <div class="receive-way-item active">打款至我的个人钱包</div>
@@ -136,7 +172,7 @@
                   <div class="day active">60</div>
                 </div>
                 <div class="custom-day">
-                  <input min="10" max="588" placeholder="输入天数（≥10）" type="number" value />
+                  <input min="10" max="588" placeholder="输入天数（≥10）" type="number" value>
                 </div>
               </div>
             </el-form-item>
@@ -146,7 +182,7 @@
             </div>
             <div class="autopay-electricity-bill">
               <div class="switch-item">
-                <el-switch v-model="Oform.renewal" active-color="#13ce66" />
+                <el-switch v-model="Oform.renewal" active-color="#13ce66"/>
                 <span class="auto-pay-title">后续电费从Matrixport钱包中自动扣除</span>
                 <span class="rule-wrap">
                   <span class="rule-link">规则说明</span>
@@ -197,9 +233,86 @@
             </div>
           </div>
           <div class="confirm-box">
-            <el-button type="primary" class="confirm-btn" disabled="disabled">
+            <el-button type="primary" class="confirm-btn" @click="showBuyShow = true">
               <span>确定</span>
             </el-button>
+            <!-- <el-button type="primary" class="confirm-btn" disabled="disabled">
+              <span>确定</span>
+            </el-button> -->
+            <el-dialog
+              :visible.sync="showBuyShow"
+              width="1100px"
+              :before-close="showBuyShow">
+              <div class="showBuyDom">
+                <div class="formDom">
+                  <div class="topInfo">
+                    <div class="titleInfo">
+                      <span>代付金额</span>
+                      4780.00
+                    </div>
+                    <div class="timeInfo">
+                      订单将在倒计时结束后失效，请及时支付
+                      <div>
+                        <span>05</span>:
+                        <span>05</span>:
+                        <span>05</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="formCon">
+                    <label>收款账户信息</label>
+                    <el-input v-model="input" placeholder="银行"></el-input>
+                    <el-input v-model="input" placeholder="账号"></el-input>
+                    <el-input v-model="input" placeholder="收款方"></el-input>
+                    <div class="button">
+                      <el-button type="primary">已支付</el-button>
+                      <el-button>以后支付</el-button>
+                    </div>
+                  </div>
+                </div>
+                <div class="orderInfo">
+                  <div class="itemInfo">
+                    <label>订单说明</label>
+                    <div>1010101010101010101010</div>
+                  </div>
+                  <div class="itemInfo">
+                    <div>
+                      <label>订单ID</label>
+                      101010101010
+                    </div>
+                    <div>
+                      <label>收款方</label>
+                      纽狮科技
+                    </div>
+                  </div>
+                  <div class="itemInfo">
+                    <div>
+                      <label>订单说明</label>
+                      100.00美元
+                    </div>
+                    <div>
+                      <label>订单说明</label>
+                      100.00美元
+                    </div>
+                    <div>
+                      <label>合计</label>
+                      <span>100.00美元</span>
+                    </div>
+                  </div>
+                  <div class="program">
+                    <label>BTC计算方案(130D * 100T)</label>
+                    <div>
+                      <span>100.00美元</span>
+                      ✕1
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="showBuyShow = false">确 定</el-button>
+              </span> -->
+            </el-dialog>
             <div>
               <el-checkbox v-model="Oform.checked">我接受</el-checkbox>
               <a href="/zh/info/121" class target="_blank" ga="order_terms_view">比特小鹿用户服务协议</a>
@@ -273,6 +386,18 @@
             <div class="tips-text">用户不得将BitDeer提供的算力用于攻击网络等违法违规行为，否则BitDeer有权强行收回您的算力。</div>
           </div>
         </div>
+        <div class="payment-instructions">
+          <h3>付款说明:</h3>
+          <ul>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+            <li>1.付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明付款说明</li>
+          </ul>
+        </div>
       </div>
       <div class="pledge-dialog">
         <div class="el-dialog__wrapper" style="display: none;">
@@ -299,7 +424,7 @@
                             aria-hidden="true"
                             class="el-checkbox__original"
                             value
-                          />
+                          >
                         </span>
                         <!---->
                       </label>
@@ -378,6 +503,7 @@
           </div>
         </div>
       </div>
+      
     </div>
   </div>
 </template>
@@ -412,8 +538,208 @@ export default {
           value: "2",
           label: "688天"
         }
-      ]
+      ],
+      showBuyShow: false
     };
+  },
+  mounted() {
+    // 加载算力信息
+    this.allShareCharts();
+    this.productShareCharts();
+    this.onlinePosCharts();
+    this.productPosCharts();
+  },
+  methods: {
+    // 总份额图表
+    allShareCharts() {
+      let chartDomNode = this.$echarts.init(
+        document.getElementById("allShareChartsID")
+      );
+      chartDomNode.setOption({
+        tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '30',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                {value: 335, name: '直接访问'},
+                {value: 310, name: '邮件营销'},
+                {value: 234, name: '联盟广告'},
+                {value: 135, name: '视频广告'},
+                {value: 1548, name: '搜索引擎'}
+            ]
+        }
+    ]
+      });
+    },
+    // 产品份额
+    productShareCharts() {
+      let chartDomNode = this.$echarts.init(
+        document.getElementById("productShareChartsID")
+      );
+      chartDomNode.setOption({
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        yAxis: {
+          type: "value"
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line"
+          }
+        ]
+      });
+    },
+    // 在线Pos值
+    onlinePosCharts() {
+      let chartDomNode = this.$echarts.init(
+        document.getElementById("onlinePosChartsID")
+      );
+      chartDomNode.setOption({
+        xAxis: {
+        type: 'category',
+        boundaryGap: false
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '30%']
+    },
+    visualMap: {
+        type: 'piecewise',
+        show: false,
+        dimension: 0,
+        seriesIndex: 0,
+        pieces: [{
+            gt: 1,
+            lt: 3,
+            color: 'rgba(0, 180, 0, 0.5)'
+        }, {
+            gt: 5,
+            lt: 7,
+            color: 'rgba(0, 180, 0, 0.5)'
+        }]
+    },
+    series: [
+        {
+            type: 'line',
+            smooth: 0.6,
+            symbol: 'none',
+            lineStyle: {
+                color: 'green',
+                width: 5
+            },
+            markLine: {
+                symbol: ['none', 'none'],
+                label: {show: false},
+                data: [
+                    {xAxis: 1},
+                    {xAxis: 3},
+                    {xAxis: 5},
+                    {xAxis: 7}
+                ]
+            },
+            areaStyle: {},
+            data: [
+                ['2019-10-10', 200],
+                ['2019-10-11', 400],
+                ['2019-10-12', 650],
+                ['2019-10-13', 500],
+                ['2019-10-14', 250],
+                ['2019-10-15', 300],
+                ['2019-10-16', 450],
+                ['2019-10-17', 300],
+                ['2019-10-18', 100]
+            ]
+        }
+    ]
+      });
+    },
+    // 产品Pos值
+    productPosCharts() {
+      let chartDomNode = this.$echarts.init(
+        document.getElementById("productPosChartsID")
+      );
+      chartDomNode.setOption({
+        xAxis: {
+        type: 'category',
+        boundaryGap: false
+    },
+    yAxis: {
+        type: 'value',
+        boundaryGap: [0, '30%']
+    },
+    visualMap: {
+        type: 'piecewise',
+        show: false,
+        dimension: 0,
+        seriesIndex: 0,
+        pieces: [{
+            gt: 1,
+            lt: 3,
+            color: 'rgba(0, 180, 0, 0.5)'
+        }, {
+            gt: 5,
+            lt: 7,
+            color: 'rgba(0, 180, 0, 0.5)'
+        }]
+    },
+    series: [
+        {
+            type: 'line',
+            smooth: 0.6,
+            symbol: 'none',
+            lineStyle: {
+                color: 'green',
+                width: 5
+            },
+            markLine: {
+                symbol: ['none', 'none'],
+                label: {show: false},
+                data: [
+                    {xAxis: 1},
+                    {xAxis: 3},
+                    {xAxis: 5},
+                    {xAxis: 7}
+                ]
+            },
+            areaStyle: {},
+            data: [
+                ['2019-10-10', 200],
+                ['2019-10-11', 400],
+                ['2019-10-12', 650],
+                ['2019-10-13', 500],
+                ['2019-10-14', 250],
+                ['2019-10-15', 300],
+                ['2019-10-16', 450],
+                ['2019-10-17', 300],
+                ['2019-10-18', 100]
+            ]
+        }
+    ]
+      });
+    },
   }
 };
 </script>
@@ -423,6 +749,219 @@ export default {
   height: 100%;
   padding-top: 30px;
 }
+.payment-instructions {
+  width: 100%;
+  height: auto;
+  background-color: #FFFFCC;
+  color: #E6A23C;
+  font-size: 15px;
+  margin: 20px 0;
+  padding: 20px;
+  h3 {
+    font-size: 20px;
+    font-weight: 800;
+  }
+  ul {
+    li {
+      width: 100%;
+      margin: 20px 10px;
+    }
+  }
+}
+.infoDom {
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0;
+  li {
+    width: 24%;
+    height: auto;
+    // padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    flex-direction: column;
+    span {
+      margin: 10px 20px;
+    }
+  }
+  li:nth-child(1) {
+    background-color: #67c23a;
+    color: #fff;
+  }
+  li:nth-child(2) {
+    background-color: #e6a23c;
+    color: #fff;
+  }
+  li:nth-child(3) {
+    background-color: #f56c6c;
+    color: #fff;
+  }
+  li:nth-child(4) {
+    background-color: #32374a;
+    color: #fff;
+  }
+}
+.chartsDom {
+  width: 100%;
+  height: auto;
+  margin: 20px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  li {
+    width: 24.5%;
+    height: auto;
+    background-color: #32374a;
+    h4 {
+      width: 100%;
+      height: auto;
+      padding: 20px;
+      color: #fff;
+    }
+    div {
+      width: 100%;
+      height: 200px;
+    }
+  }
+  li:nth-child(2) {
+    margin-right: 2%;
+  }
+}
+.showBuyDom {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  .formDom {
+    width: 710px;
+    .topInfo {
+      display: flex;
+      align-items: flex-end;
+      justify-content: space-between;
+      padding: 20px 0;
+      .titleInfo {
+        font-size: 20px;
+        color: #F56C6C;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+        span {
+          font-weight: 800;
+          color: #343434;
+        }
+      }
+      .timeInfo {
+        display: flex;
+          align-items: center;
+          justify-content: center;
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: #FF3300;
+          span {
+            width: 40px;
+            height: 30px;
+            font-size: 20px;
+            margin: 0 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #F56C6C;
+          }
+        }
+      }
+    }
+    .formCon {
+      width: 100%;
+      height: auto;
+      border-top: 1px solid #999;
+      padding: 20px 0;
+      display: flex;
+        align-items: flex-start;
+        justify-content: space-between;
+        flex-direction: column;
+      label {
+        font-size: 15px;
+        color: #343434;
+      }
+      .el-input {
+        width: 250px;
+        margin: 10px 50px;
+      }
+      .button {
+        width: 100%;
+        height: auto;
+        padding-top: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .el-button {
+          width: 200px;
+          height: 80px;
+          display: flex;
+        align-items: center;
+        justify-content: center;
+        }
+      }
+    }
+  }
+  .orderInfo {
+    width: 300px;
+    height: auto;
+    padding: 20px;
+    border: 1px solid #ddd;
+    font-size: 12px;
+    color: #343434;
+    .itemInfo {
+      width: 100%;
+      height: auto;
+      border-bottom: 1px solid #ddd; 
+      text-align: left;
+      margin: 25px 0;
+      label {
+        color: #999;
+      }
+      span {
+        color: #3c63e3;
+      }
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 10px 0;
+      }
+    }
+    .program {
+        margin-top: 20px;
+        padding: 20px;
+        width: 100%;
+        height: auto;
+        border-radius: 10px;
+        background-color: #ddd;
+        text-align: left;
+        label {
+          font-size: 15px;
+          font-weight: 700;
+          margin: 10px 0;
+        }
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin: 10px 0;
+          span {
+            color: #3c63e3;
+          }
+        }
+      }
+  }
+}
+
 .order-summary {
   width: 1200px;
   margin: 0 auto 30px;
